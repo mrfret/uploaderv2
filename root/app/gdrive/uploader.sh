5ccc87cc4504825c4b91db6d56c83ee4ee02b8fd
doob187 <60312740+doob187@users.noreply.github.com>
2020-05-01 14:49:48 +0200

    Update uploader.sh (#7)
    * Update uploader.sh
    
    * Update uploader.sh

--- root/app/gdrive/uploader.sh
+++ root/app/gdrive/uploader.sh
@@ -174,7 +174,7 @@ while true; do
         done
         log "Finished looking for files, sleeping 5 secs"
     else
-        log "Nothing to upload, sleeping 5 secs" 2>/dev/null
+        log "Nothing to upload, sleeping 5 secs" > /dev/null 2>&1
     fi
     sleep 5
 done
