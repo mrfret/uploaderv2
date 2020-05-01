9f602996151820e16651589574514574f96257f9
doob187 <amderkum@gmail.com>
2020-05-01 18:46:36 +0200

    revert

--- root/app/gdrive/uploader.sh
+++ root/app/gdrive/uploader.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/with-contenv bash
 # shellcheck shell=bash
-
 # Copyright (c) 2019, PhysK
 # All rights reserved.
-
 # Logging Function
 function log() {
     echo "[Uploader] ${1}"
 }
-
 #Make sure all the folders we need are created
 mkdir -p /config/pid/
 mkdir -p /config/json/
@@ -17,7 +14,6 @@ mkdir -p /config/vars/
 mkdir -p /config/vars/gdrive/
 downloadpath=/move
 MOVE_BASE=${MOVE_BASE:-/}
-
 # Check encryption status
 # Check encryption status
 ENCRYPTED=${ENCRYPTED:-false}
@@ -26,12 +22,10 @@ if [[ "${ENCRYPTED}" == "false" ]]; then
           ENCRYPTED=true
     fi
 fi
-
 ADDITIONAL_IGNORES=${ADDITIONAL_IGNORES}
 if [ "${ADDITIONAL_IGNORES}" == 'null' ]; then
     ADDITIONAL_IGNORES=""
 fi
-
 UPLOADS=${UPLOADS}
 if [ "${UPLOADS}" == 'null' ]; then
    UPLOADS="8"
@@ -40,23 +34,18 @@ elif [ "${UPLOADS}" -ge '20' ]; then
 else
    UPLOADS=${UPLOADS}
 fi
-
 #Header
 log "Upload Docker is Starting"
 log "Started for the First Time - Cleaning up if from reboot"
 log "Uploads is set to ${UPLOADS}"
-
 # Remove left over webui and transfer files
 rm -f /config/pid/*
 rm -f /config/json/*
 rm -f /config/logs/*
-
 # delete any lock files for files that failed to upload
 find ${downloadpath} -type f -name '*.lck' -delete
 log "Cleaned up - Sleeping 10 secs"
 sleep 10
-
-
 # Check if BC is installed
 if [ "$(echo "10 + 10" | bc)" == "20" ]; then
     log "BC Found! All good :)"
@@ -70,10 +59,8 @@ if [ -e /config/vars/lastGDSA ]; then
 else
     GDSAAMOUNT=0
 fi
-
 # Run Loop
 while true; do
-
     mapfile -t timestamps < <(eval find /config/vars/gdrive -type f)
     for file in "${timestamps[@]}";
     do
@@ -111,9 +98,8 @@ while true; do
                         log "File is still getting bigger ${i}"
                         continue
                     fi
-
                     # Check if we have any upload slots available
-                    # shellcheck disable=SC2010
+			        # shellcheck disable=SC2010
                     TRANSFERS=$(ls -la /config/pid/ | grep -c trans)
                     # shellcheck disable=SC2086
                     if [ ! ${TRANSFERS} -ge ${UPLOADS} ]; then
@@ -121,33 +107,26 @@ while true; do
                             log "Starting upload of ${i}"
                             # Append filesize to GDSAAMOUNT
                             GDSAAMOUNT=$(echo "${GDSAAMOUNT} + ${FILESIZE2}" | bc)
-
                             # Set gdrive as crypt or not
                             if [ ${ENCRYPTED} == "true" ]; then
                                 GDSA_TO_USE="gcrypt"
                             else
                                 GDSA_TO_USE="gdrive"
                             fi
-
                             # Increase or reset $GDSAUSE?
                             # shellcheck disable=SC2086
                             if [ ${GDSAAMOUNT} -gt "783831531520" ]; then
                                 log "gdrive has hit 730GB uploads will resume when they can ( ︶︿︶)_╭∩╮"
                                 break
                             fi
-
                             # Add filesize to file
                             echo "${FILESIZE2}" > "/config/vars/gdrive/$(echo "$(date +%s) + 86400" | bc)"
-
                             # Run upload script demonised
                             /app/gdrive/upload.sh "${i}" "${GDSA_TO_USE}" &
-
                             PID=$!
                             FILEBASE=$(basename "${i}")
-
                             # Add transfer to pid directory
                             echo "${PID}" > "/config/pid/${FILEBASE}.trans"
-
                             log "gdrive is now $(echo "${GDSAAMOUNT}/1024/1024/1024" | bc -l)"
 
                             # Record GDSA transfered in case of crash/reboot
@@ -174,7 +153,7 @@ while true; do
         done
         log "Finished looking for files, sleeping 5 secs"
     else
-        log "Nothing to upload, sleeping 5 secs" > /dev/null 2>&1
+        log "Nothing to upload, sleeping 5 secs"
     fi
     sleep 5
 done
--- root/app/tdrive/uploader.sh
+++ root/app/tdrive/uploader.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/with-contenv bash
 # shellcheck shell=bash
-
 # Copyright (c) 2019, PhysK
 # All rights reserved.
-
 # Logging Function
 function log() {
     echo "[Uploader] ${1}"
 }
-
 #Make sure all the folders we need are created
 path=/config/keys/
 mkdir -p /config/pid/
@@ -17,7 +14,6 @@ mkdir -p /config/logs/
 mkdir -p /config/vars/
 downloadpath=/move
 MOVE_BASE=${MOVE_BASE:-/}
-
 # Check encryption status
 ENCRYPTED=${ENCRYPTED:-false}
 if [[ "${ENCRYPTED}" == "false" ]]; then
@@ -25,12 +21,10 @@ if [[ "${ENCRYPTED}" == "false" ]]; then
           ENCRYPTED=true
     fi
 fi
-
 ADDITIONAL_IGNORES=${ADDITIONAL_IGNORES}
 if [ "${ADDITIONAL_IGNORES}" == 'null' ]; then
     ADDITIONAL_IGNORES=""
 fi
-
 UPLOADS=${UPLOADS}
 if [ "${UPLOADS}" == 'null' ]; then
    UPLOADS="8"
@@ -39,23 +33,18 @@ elif [ "${UPLOADS}" -ge '20' ]; then
 else
    UPLOADS=${UPLOADS}
 fi
-
-
 #Header
 log "Upload Docker is Starting"
 log "Started for the First Time - Cleaning up if from reboot"
 log "Uploads is set to ${UPLOADS}"
-
 # Remove left over webui and transfer files
 rm -f /config/pid/*
 rm -f /config/json/*
 rm -f /config/logs/*
-
 # delete any lock files for files that failed to upload
 find ${downloadpath} -type f -name '*.lck' -delete
 log "Cleaned up - Sleeping 10 secs"
 sleep 10
-
 #### Generates the GDSA List from the Processed Keys
 # shellcheck disable=SC2003
 # shellcheck disable=SC2006
@@ -66,14 +55,12 @@ sleep 10
 GDSAARRAY=(`ls -la ${path} | awk '{print $9}' | egrep '(PG|GD|GS)'`)
 # shellcheck disable=SC2003
 GDSACOUNT=$(expr ${#GDSAARRAY[@]} - 1)
-
 # Check to see if we have any keys
 # shellcheck disable=SC2086
 if [ ${GDSACOUNT} -lt 1 ]; then
     log "No accounts found to upload with, Exit"
     exit 1
 fi
-
 # Check if BC is installed
 if [ "$(echo "10 + 10" | bc)" == "20" ]; then
     log "BC Found! All good :)"
@@ -89,7 +76,6 @@ else
     GDSAUSE=0
     GDSAAMOUNT=0
 fi
-
 # Run Loop
 while true; do
     #Find files to transfer
@@ -114,7 +100,6 @@ while true; do
                         log "File is still getting bigger ${i}"
                         continue
                     fi
-
                     # Check if we have any upload slots available
                     # shellcheck disable=SC2010
                     TRANSFERS=$(ls -la /config/pid/ | grep -c trans)
@@ -124,23 +109,18 @@ while true; do
                             log "Starting upload of ${i}"
                             # Append filesize to GDSAAMOUNT
                             GDSAAMOUNT=$(echo "${GDSAAMOUNT} + ${FILESIZE2}" | bc)
-
                             # Set gdsa as crypt or not
                             if [ ${ENCRYPTED} == "true" ]; then
                                 GDSA_TO_USE="${GDSAARRAY[$GDSAUSE]}C"
                             else
                                 GDSA_TO_USE="${GDSAARRAY[$GDSAUSE]}"
                             fi
-
                             # Run upload script demonised
                             /app/tdrive/upload.sh "${i}" "${GDSA_TO_USE}" &
-
                             PID=$!
                             FILEBASE=$(basename "${i}")
-
                             # Add transfer to pid directory
                             echo "${PID}" > "/config/pid/${FILEBASE}.trans"
-
                             # Increase or reset $GDSAUSE?
                             # shellcheck disable=SC2086
                             if [ ${GDSAAMOUNT} -gt "783831531520" ]; then
@@ -179,7 +159,7 @@ while true; do
         done
         log "Finished looking for files, sleeping 5 secs"
     else
-        log "Nothing to upload, sleeping 5 secs" > /dev/null 2>&1
+        log "Nothing to upload, sleeping 5 secs"
     fi
     sleep 5
 done
