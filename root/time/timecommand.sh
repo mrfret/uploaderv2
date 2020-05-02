41ec1fda49261057bae0cc02f80be8f2ba89b2c5
doob187 <amderkum@gmail.com>
2020-05-02 21:09:40 +0200

    Revert "timecommand"
    This reverts commit ad7ece0e831cd00f6945b4c65e99043612e67b70.

---
+++ root/time/timecommand.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Set the timezone. Base image does not contain the setup-timezone script, so an alternate way is used.
+if [ "$SET_CONTAINER_TIMEZONE" = "true" ]; then
+    cp /usr/share/zoneinfo/${CONTAINER_TIMEZONE} /etc/localtime && \
+	echo "${CONTAINER_TIMEZONE}" >  /etc/timezone && \
+	echo "Container timezone set to: $CONTAINER_TIMEZONE"
+else
+	echo "Container timezone not modified"
+fi
