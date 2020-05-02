ad7ece0e831cd00f6945b4c65e99043612e67b70
doob187 <amderkum@gmail.com>
2020-05-02 20:38:07 +0200

    timecommand

--- root/time/timecommand.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Set the timezone. Base image does not contain the setup-timezone script, so an alternate way is used.
-if [ "$SET_CONTAINER_TIMEZONE" = "true" ]; then
-    cp /usr/share/zoneinfo/${CONTAINER_TIMEZONE} /etc/localtime && \
-	echo "${CONTAINER_TIMEZONE}" >  /etc/timezone && \
-	echo "Container timezone set to: $CONTAINER_TIMEZONE"
-else
-	echo "Container timezone not modified"
-fi
