4ebc9d16f7e9285f9dcb65dbfe5c6864a27baa07
doob187 <amderkum@gmail.com>
2020-05-09 21:08:52 +0200

    uppstream 3
        log "Upload complete for ${FILEDIR}/${FILEBASE} | SIZE : ${HRFILESIZE} | Speed : ${BWLIMIT}" >"${FILEBASE}.discord"

--- root/app/tdrive/upload.sh
+++ root/app/tdrive/upload.sh
@@ -64,7 +64,7 @@ fi
 echo "{\"filedir\": \"/${FILEDIR}\",\"filebase\": \"${FILEBASE}\",\"filesize\": \"${HRFILESIZE}\",\"status\": \"done\",\"gdsa\": \"${GDSA}\",\"starttime\": \"${STARTTIME}\",\"endtime\": \"${ENDTIME}\"}" >"${JSONFILE}"
 ### send note to discod 
   if [ ${DISCORD_WEBHOOK_URL} != 'null' ]; then
-    log "Upload complete for ${FILEDIR}/${FILEBASE}" >"${FILEBASE}.discord"
+    log "Upload complete for ${FILEDIR}/${FILEBASE} | SIZE : ${HRFILESIZE} | Speed : ${BWLIMIT}" >"${FILEBASE}.discord"
     message=$(cat ${FILEBASE}.discord)
     msg_content=\"$message\"
     USERNAME=\"${DISCORD_NAME_OVERRIDE}\"
