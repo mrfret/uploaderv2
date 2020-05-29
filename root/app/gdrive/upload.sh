85712a97c4ccd7013a1670530eb60bcfd7a8adc5
doob187 <amderkum@gmail.com>
2020-05-29 23:39:16 +0200

    Update upload.sh

--- root/app/gdrive/upload.sh
+++ root/app/gdrive/upload.sh
@@ -61,18 +61,16 @@ else
     BWLIMIT=""
     BWLIMITSPEED="no LIMIT was set"
 fi
-#create and chmod the log file so that webui can read it
 touch "${LOGFILE}"
 chmod 777 "${LOGFILE}"
 #update json file for Uploader GUI
 echo "{\"filedir\": \"${FILEDIR}\",\"filebase\": \"${FILEBASE}\",\"filesize\": \"${HRFILESIZE}\",\"status\": \"uploading\",\"logfile\": \"${LOGFILE}\",\"gdsa\": \"${GDSA}\"}" >"${JSONFILE}"
 log "[Upload] Starting Upload"
 rclone moveto --tpslimit 6 --checkers=${CHECKERS} \
-    --config /config/rclone-docker.conf \
-    --log-file="${LOGFILE}" --log-level INFO --stats 2s \
-    --drive-chunk-size=${CHUNK}M ${BWLIMIT} \
-    --drive-stop-on-upload-limit \
-    "${FILE}" "${REMOTE}:${FILEDIR}/${FILEBASE}"
+       --config /config/rclone-docker.conf \
+       --log-file="${LOGFILE}" --log-level INFO --stats 2s \
+       --drive-chunk-size=${CHUNK}M ${BWLIMIT} \
+       "${FILE}" "${REMOTE}:${FILEDIR}/${FILEBASE}"
 ENDTIME=$(date +%s)
 if [ "${RC_ENABLED}" == "true" ]; then
     sleep 10s
