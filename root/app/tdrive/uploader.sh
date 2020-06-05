a8033a7647e68e2ebfbaaec01cbd5a1aca27ae78
doob187 <60312740+doob187@users.noreply.github.com>
2020-06-05 15:54:43 +0200

    Update uploader.sh

--- root/app/tdrive/uploader.sh
+++ root/app/tdrive/uploader.sh
@@ -4,7 +4,7 @@
 # All rights reserved.
 # Logging Function
 function log() {
-    echo "[Uploader] ${1}"
+ echo "[Uploader] ${1}"
 }
 #Make sure all the folders we need are created
 path=/config/keys/
@@ -18,13 +18,13 @@ MOVE_BASE=${MOVE_BASE:-/}
 # Check encryption status
 ENCRYPTED=${ENCRYPTED:-false}
 if [[ "${ENCRYPTED}" == "false" ]]; then
-    if grep -q GDSA01C /config/rclone-docker.conf && grep -q GDSA02C /config/rclone-docker.conf; then
-          ENCRYPTED=true
-    fi
+ if grep -q GDSA01C /config/rclone-docker.conf && grep -q GDSA02C /config/rclone-docker.conf; then
+    ENCRYPTED=true
+ fi
 fi
 ADDITIONAL_IGNORES=${ADDITIONAL_IGNORES}
 if [ "${ADDITIONAL_IGNORES}" == 'null' ]; then
-    ADDITIONAL_IGNORES=""
+   ADDITIONAL_IGNORES=""
 fi
 UPLOADS=${UPLOADS}
 if [ "${UPLOADS}" == 'null' ]; then
@@ -38,20 +38,19 @@ DISCORD_WEBHOOK_URL=${DISCORD_WEBHOOK_URL}
 DISCORD_ICON_OVERRIDE=${DISCORD_ICON_OVERRIDE}
 DISCORD_NAME_OVERRIDE=${DISCORD_NAME_OVERRIDE}
 DISCORD="/config/discord/startup.discord"
-
-  if [ ${DISCORD_WEBHOOK_URL} != 'null' ]; then
-    echo "Upload Docker is Starting \nStarted for the First Time \nCleaning up if from reboot \nUploads is set to ${UPLOADS}" >"${DISCORD}"
-    message=$(cat "${DISCORD}")
-    msg_content=\"$message\"
-    USERNAME=\"${DISCORD_NAME_OVERRIDE}\"
-    IMAGE=\"${DISCORD_ICON_OVERRIDE}\"
-    DISCORD_WEBHOOK_URL="${DISCORD_WEBHOOK_URL}"
-    curl -H "Content-Type: application/json" -X POST -d "{\"username\": $USERNAME, \"avatar_url\": $IMAGE, \"content\": $msg_content}" $DISCORD_WEBHOOK_URL
-  else
-    log "Upload Docker is Starting"
-    log "Started for the First Time - Cleaning up if from reboot"
-    log "Uploads is set to ${UPLOADS}"
-  fi
+if [ ${DISCORD_WEBHOOK_URL} != 'null' ]; then
+  echo "Upload Docker is Starting \nStarted for the First Time \nCleaning up if from reboot \nUploads is set to ${UPLOADS}" >"${DISCORD}"
+  message=$(cat "${DISCORD}")
+  msg_content=\"$message\"
+  USERNAME=\"${DISCORD_NAME_OVERRIDE}\"
+  IMAGE=\"${DISCORD_ICON_OVERRIDE}\"
+  DISCORD_WEBHOOK_URL="${DISCORD_WEBHOOK_URL}"
+  curl -H "Content-Type: application/json" -X POST -d "{\"username\": $USERNAME, \"avatar_url\": $IMAGE, \"content\": $msg_content}" $DISCORD_WEBHOOK_URL
+else
+  log "Upload Docker is Starting"
+  log "Started for the First Time - Cleaning up if from reboot"
+  log "Uploads is set to ${UPLOADS}"
+fi
 # Remove left over webui and transfer files
 rm -f /config/pid/*
 rm -f /config/json/*
@@ -61,7 +60,6 @@ rm -f /config/discord/*
 find ${downloadpath} -type f -name '*.lck' -delete
 log "Cleaned up - Sleeping 10 secs"
 sleep 10
-
 #### Generates the GDSA List from the Processed Keys
 # shellcheck disable=SC2003
 # shellcheck disable=SC2006
@@ -75,23 +73,16 @@ GDSACOUNT=$(expr ${#GDSAARRAY[@]} - 1)
 # Check to see if we have any keys
 # shellcheck disable=SC2086
 if [ ${GDSACOUNT} -lt 1 ]; then
-    log "No accounts found to upload with, Exit"
-    exit 1
-fi
-# Check if BC is installed
-if [ "$(echo "10 + 10" | bc)" == "20" ]; then
-    log "BC Found! All good :)"
-else
-    log "BC Not installed, Exit"
-    exit 2
+   log "No accounts found to upload with, Exit" 
+   exit 1
 fi
 # Grabs vars from files
 if [ -e /config/vars/lastGDSA ]; then
-    GDSAUSE=$(cat /config/vars/lastGDSA)
-    GDSAAMOUNT=$(cat /config/vars/gdsaAmount)
+   GDSAUSE=$(cat /config/vars/lastGDSA)
+   GDSAAMOUNT=$(cat /config/vars/gdsaAmount)
 else
-    GDSAUSE=0
-    GDSAAMOUNT=0
+   GDSAUSE=0
+   GDSAAMOUNT=0
 fi
 # Run Loop
 while true; do
@@ -105,79 +96,71 @@ while true; do
             FILEDIR=$(dirname "${i}" | sed "s#${downloadpath}${MOVE_BASE}##g")
             # If file has a lockfile skip
             if [ -e "${i}.lck" ]; then
-                log "Lock File found for ${i}"
-                continue
+               log "Lock File found for ${i}"
+               continue
             else
                 if [ -e "${i}" ]; then
                     # Check if file is still getting bigger
                     FILESIZE1=$(stat -c %s "${i}")
-                    sleep 3
                     FILESIZE2=$(stat -c %s "${i}")
                     if [ "$FILESIZE1" -ne "$FILESIZE2" ]; then
-                        log "File is still getting bigger ${i}"
-                        continue
+                      continue
                     fi
-                    # Check if we have any upload slots available
                     # shellcheck disable=SC2010
                     TRANSFERS=$(ls -la /config/pid/ | grep -c trans)
                     # shellcheck disable=SC2086
                     if [ ! ${TRANSFERS} -ge ${UPLOADS} ]; then
-                        if [ -e "${i}" ]; then
-                            log "Starting upload of ${i}"
-                            # Append filesize to GDSAAMOUNT
-                            GDSAAMOUNT=$(echo "${GDSAAMOUNT} + ${FILESIZE2}" | bc)
-                            # Set gdsa as crypt or not
-                            if [ ${ENCRYPTED} == "true" ]; then
-                                GDSA_TO_USE="${GDSAARRAY[$GDSAUSE]}C"
-                            else
-                                GDSA_TO_USE="${GDSAARRAY[$GDSAUSE]}"
-                            fi
-                            # Run upload script demonised
-                            /app/uploader/upload.sh "${i}" "${GDSA_TO_USE}" &
-                            PID=$!
-                            FILEBASE=$(basename "${i}")
-                            # Add transfer to pid directory
-                            echo "${PID}" > "/config/pid/${FILEBASE}.trans"
-
-                            # Increase or reset $GDSAUSE?
-                            # shellcheck disable=SC2086
-                            if [ ${GDSAAMOUNT} -gt "783831531520" ]; then
-                                log "${GDSAARRAY[$GDSAUSE]} has hit 730GB switching to next SA"
-                                if [ "${GDSAUSE}" -eq "${GDSACOUNT}" ]; then
-                                    GDSAUSE=0
-                                    GDSAAMOUNT=0
-                                else
-                                    GDSAUSE=$(("${GDSAUSE}" + 1))
-                                    GDSAAMOUNT=0
-                                fi
-                                # Record next GDSA in case of crash/reboot
-                                echo "${GDSAUSE}" >/config/vars/lastGDSA
-                            fi
-                            log "${GDSAARRAY[${GDSAUSE}]} is now $(echo "${GDSAAMOUNT}/1024/1024/1024" | bc -l)"
-                            # Record GDSA transfered in case of crash/reboot
-                            echo "${GDSAAMOUNT}" >/config/vars/gdsaAmount
-                        else
-                            log "File ${i} seems to have dissapeared"
-                        fi
-                    else
-                        log "Already ${UPLOADS} transfers running, waiting for next loop"
-                        break
-                    fi
-                else
-                    log "File not found: ${i}"
-                    continue
-                fi
-            fi
-            if [[ -d "/mnt/tdrive1/${FILEDIR}" || -d "/mnt/tdrive2/${FILEDIR}" ]]; then
-                continue
-            else
-                log "Sleeping 5s before looking at next file"
-                sleep 5
-            fi
-        done
-        log "Finished looking for files, sleeping 5 secs"
-    else
-        log "Nothing to upload, sleeping 5 secs"
-    fi
-    sleep 5
+                       if [ -e "${i}" ]; then
+                          log "Starting upload of ${i}"
+                           GDSAAMOUNT=$(echo "${GDSAAMOUNT} + ${FILESIZE2}" | bc)
+                           # Set gdsa as crypt or not
+                           if [ ${ENCRYPTED} == "true" ]; then
+                              GDSA_TO_USE="${GDSAARRAY[$GDSAUSE]}C"
+                           else
+                              GDSA_TO_USE="${GDSAARRAY[$GDSAUSE]}"
+                           fi
+                           /app/uploader/upload.sh "${i}" "${GDSA_TO_USE}" &
+                           PID=$!
+                           FILEBASE=$(basename "${i}")
+                           echo "${PID}" > "/config/pid/${FILEBASE}.trans"
+                           # shellcheck disable=SC2086
+                           if [ ${GDSAAMOUNT} -gt "783831531520" ]; then
+                              log "${GDSAARRAY[$GDSAUSE]} has hit 730GB switching to next SA"
+                              if [ "${GDSAUSE}" -eq "${GDSACOUNT}" ]; then
+                                 GDSAUSE=0
+                                 GDSAAMOUNT=0
+                              else
+                                 GDSAUSE=$(("${GDSAUSE}" + 1))
+                                 GDSAAMOUNT=0
+                              fi
+                              # Record next GDSA in case of crash/reboot
+                              echo "${GDSAUSE}" >/config/vars/lastGDSA
+                           fi
+                           log "${GDSAARRAY[${GDSAUSE}]} is now $(echo "${GDSAAMOUNT}/1024/1024/1024" | bc -l)"
+                           # Record GDSA transfered in case of crash/reboot
+                           echo "${GDSAAMOUNT}" >/config/vars/gdsaAmount
+                       else
+                          log "File ${i} seems to have dissapeared"
+                       fi
+                   else
+                      log "Already ${UPLOADS} transfers running, waiting for next loop"
+                      break
+                   fi
+               else
+                  log "File not found: ${i}"
+                  continue
+               fi
+           fi
+           if [[ -d "/mnt/tdrive1/${FILEDIR}" || -d "/mnt/tdrive2/${FILEDIR}" ]]; then
+              continue
+           else
+              log "Sleeping 5s before looking at next file"
+              sleep 5
+           fi
+       done
+       log "Finished looking for files, sleeping 5 secs"
+   else
+       log "Nothing to upload, sleeping 5 secs"
+   fi
+   sleep 5
 done
