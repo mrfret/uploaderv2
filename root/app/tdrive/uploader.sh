b1915591da5e08f8aeaba1840702235cf9c95cb3
doob187 <amderkum@gmail.com>
2020-06-26 19:00:21 +0200

    plex function / uploader function

--- root/app/tdrive/uploader.sh
+++ root/app/tdrive/uploader.sh
@@ -97,7 +97,10 @@ while true; do
                            else
                               GDSA_TO_USE="${GDSAARRAY[$GDSAUSE]}"
                            fi
-                           /app/uploader/upload.sh "${i}" "${GDSA_TO_USE}" &
+                           bwlimitpart "${i}" &
+                           FILEBASE=$(basename "${i}")
+                           sleep 1
+                           uploader "${i}" "${GDSA_TO_USE}" &
                            PID=$!
                            FILEBASE=$(basename "${i}")
                            echo "${PID}" > "/config/pid/${FILEBASE}.trans"
