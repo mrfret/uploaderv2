2e80bc7f702d0f143a73569b9bd2e4d079cafd91
doob187 <60312740+doob187@users.noreply.github.com>
2020-07-18 07:47:38 +0200

    Update serverside.sh

--- root/app/serverside/serverside.sh
+++ root/app/serverside/serverside.sh
@@ -9,20 +9,27 @@ function log() {
     echo "[Server Side] ${1}"
 }
 
+if pidof -o %PPID -x "$0"; then
+    exit 1
+fi
+
 SVLOG="serverside"
 RCLONEDOCKER="/config/rclone-docker.conf"
 LOGFILE="/config/logs/${SVLOG}.log"
 truncate -s 0 /config/logs/${SVLOG}.log
 sunday="(date '+%A')"
 yanow="Sunday"
-
+PID="/config/pid"
+echo "lock" >"${DISCORD}"
+DISCORD="/config/discord/${SVLOG}.discord"
+DISCORD_WEBHOOK_URL=${DISCORD_WEBHOOK_URL}
 #####
 SERVERSIDEDRIVE=${SERVERSIDEDRIVE:-null}
 SERVERSIDE=${SERVERSIDE:-null}
 REMOTEDRIVE=${REMOTEDRIVE:-null}
 SERVERSIDEMINAGE=${SERVERSIDEMINAGE:-null}
 #####
-if [[ "${SERVERSIDE}" == "false" ]]; then
+if [[ "${SERVERSIDE}" == "null" ]]; then
  if grep -q server_side** ${RCLONEDOCKER} ; then
     SERVERSIDE=true
  else 
@@ -60,18 +67,36 @@ while true; do
   if [[ ${sunday} == Sunday ]]; then
      sleep 5
      if [ ${SERVERSIDE} == "true" ]; then
+         echo "${SVLOG}" > "/config/pid/${SVLOG}.serverside" 
+         echo "${SVLOG}" > "/config/pid/${SVLOG}.lck"
+         STARTTIME=$(date +now)
          log "Starting Server-Side move from ${REMOTEDRIVE} to ${SERVERSIDEDRIVE}"
          rclone move --checkers 4 --transfers 2 \
                 --config=${RCLONEDOCKER} --log-file="${LOGFILE}" --log-level INFO --stats 5s \
-                --no-traverse ${SERVERSIDEAGE} --fast-list \
+                --no-traverse ${SERVERSIDEAGE} --fast-list --delete-empty-src-dirs \
                 "${REMOTEDRIVE}:" "${SERVERSIDEDRIVE}:"
-         log "Finished Server-Side move from ${REMOTEDRIVE} to ${SERVERSIDEDRIVE}"
-     fi
-  else
+          sleep 5
+          ENDTIME=$(date +now)
+          if [ ${DISCORD_WEBHOOK_URL} != 'null' ]; then
+             TITEL="Server-Side Move"
+             DISCORD_ICON_OVERRIDE=${DISCORD_ICON_OVERRIDE}              
+             DISCORD_NAME_OVERRIDE=${DISCORD_NAME_OVERRIDE}
+             # shellcheck disable=SC2006 
+             echo "log "Finished Server-Side move from ${REMOTEDRIVE} to ${SERVERSIDEDRIVE} \nStarted : ${STARTTIME} \nFinished : ${ENDTIME}" >"${DISCORD}"
+             msg_content=$(cat "${DISCORD}")
+             curl -H "Content-Type: application/json" -X POST -d "{\"username\": \"${DISCORD_NAME_OVERRIDE}\", \"avatar_url\": \"${DISCORD_ICON_OVERRIDE}\", \"embeds\": [{ \"title\": \"${TITEL}\", \"description\": \"$msg_content\" }]}" $DISCORD_WEBHOOK_URL
+          else
+             log "Finished Server-Side move from ${REMOTEDRIVE} to ${SERVERSIDEDRIVE}"
+          fi
+    else
      log "Next Start on ${yanow}"
      log "Server-Side move from ${REMOTEDRIVE} to ${SERVERSIDEDRIVE}"
         if [[ ${sunday} != Sunday ]]; then
             sleep 24h || break
         fi
-  fi 
+  fi
+  rm -f "${PID}/${SVLOG}.lck" \
+       "${LOGFILE}" \
+       "${PID}/${SVLOG}.serverside" \
+       "${DISCORD}"
 done
