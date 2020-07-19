4e525b15956c3a6eedcaa9e4b9142257c7f63b03
doob187 <60312740+doob187@users.noreply.github.com>
2020-07-19 04:53:19 +0200

    Update serverside.sh

--- root/app/serverside/serverside.sh
+++ root/app/serverside/serverside.sh
@@ -9,6 +9,33 @@ function log() {
     echo "[Server Side] ${1}"
 }
 
+##basic check of working###
+RCLONEDOCKER=/config/rclone-docker.conf
+if grep -q server_side** ${RCLONEDOCKER}; then
+   echo "-->> Server_side is added <<-- "
+ if grep -q "\[tcrypt\]" ${RCLONEDOCKER} && grep -q "\[gcrypt\]" ${RCLONEDOCKER}; then
+     echo " -->> Check for booth salt passwords <<--"
+     rccommand1=$(rclone reveal $(cat ${RCLONEDOCKER} | awk '$1 == "password" {print $3}' | head -n 1 | tail -n 1))
+     rccommand2=$(rclone reveal $(cat ${RCLONEDOCKER} | awk '$1 == "password" {print $3}' | head -n 2 | tail -n 1))
+     rccommand3=$(rclone reveal $(cat ${RCLONEDOCKER} | awk '$1 == "password2" {print $3}' | head -n 1 | tail -n 1))
+     rccommand4=$(rclone reveal $(cat ${RCLONEDOCKER} | awk '$1 == "password2" {print $3}' | head -n 2 | tail -n 1))
+   if [[ "${rccommand1}" == "${rccommand2}" && "${rccommand3}" == "${rccommand4}" ]]; then
+      echo " --->> Server_side can be used <<-- "
+      echo " --->> TCrypt and GCrypt used the same password <<-- "
+   else
+      exit
+      echo " -->> Server_side can't be used <<-- "
+      echo " -->> TCrypt and GCrypt dont used the same password <<-- "
+   fi
+  fi
+else
+   echo "-->> Server_side is not included <<--"
+   echo "-->> skipping <<--"
+   exit
+fi
+
+
+###execute part 
 SVLOG="serverside"
 RCLONEDOCKER="/config/rclone-docker.conf"
 LOGFILE="/config/logs/${SVLOG}.log"
