943e81475d928af2d639217b12ece7259a417441
doob187 <60312740+doob187@users.noreply.github.com>
2020-08-25 13:43:28 +0200

    Update functions.sh

--- root/app/functions/functions.sh
+++ root/app/functions/functions.sh
@@ -127,12 +127,12 @@ mapfile -t mounts < <(eval rclone listremotes --config=${config} | grep "$filter
 ## function source end
 for i in ${mounts[@]}; do
   #echo; echo STARTING DEDUPE of identical files from $i; echo
-  rclone dedupe skip $i: -v --config=${config} --drive-use-trash=false --no-traverse --transfers=50
+  rclone dedupe skip $i: --config=${config} --drive-use-trash=false --no-traverse --transfers=50
   #echo; echo REMOVING EMPTY DIRECTORIES from $i; echo
-  rclone rmdirs $i: -v --config=${config} --drive-use-trash=false --fast-list --transfers=50
+  rclone rmdirs $i: --config=${config} --drive-use-trash=false --fast-list --transfers=50
   #echo; echo PERMANENTLY DELETING TRASH from $i; echo
   rclone delete $i: --config=${config} --fast-list --drive-trashed-only --drive-use-trash=false -v --transfers 50
-  rclone cleanup $i: -v --config=${config}
+  rclone cleanup $i: --leave-root --config=${config}
 done
 }
 #<|EOF|>#
