fa335505b6c67f9fd82586c022969e224c8136b9
doob187 <amderkum@gmail.com>
2020-09-07 19:45:58 +0200

    up

--- root/app/serverside/serverside.sh
+++ root/app/serverside/serverside.sh
@@ -121,6 +121,11 @@ while true; do
                  --log-file="${LOGFILE}" --log-level INFO --stats 10s \
                  --no-traverse ${SERVERSIDEAGE} \
                  "${REMOTEDRIVE}:" "${SERVERSIDEDRIVE}:"
+   rclone rmdirs "${REMOTEDRIVE}:" --config="${RCLONEDOCKER}" \
+                 --drive-use-trash=false \
+				 --fast-list \
+				 --transfers=50 \
+				 --user-agent="SomeLegitUserAgent" 
    ENDTIME=$(date +%s)
    if [ ${DISCORD_WEBHOOK_URL} != 'null' ]; then
       TITEL="Server-Side Move"
