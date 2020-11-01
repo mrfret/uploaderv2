2da7c26810a92d61120b1e60943de4c02f729b10
doob187 <60312740+doob187@users.noreply.github.com>
2020-11-01 19:14:56 +0100

    Update uploader.sh
    Stupid file

--- root/app/tdrive/uploader.sh
+++ root/app/tdrive/uploader.sh
@@ -104,7 +104,7 @@ while true; do
                     else
                         UPLOADSPEED=$(vnstat -i eth0 -tr 5 | awk '$1 == "tx" {print $2}' | sed -r 's/([^0-9]*([0-9]*)){1}.*/\2/')
                     fi
-                     if [[ ! ${TRANSFERS} -ge 4 && ! ${BWLIMITSET} -ge ${UPLOADSPEED} ]]; then
+                     if [[ ! ${TRANSFERS} -ge 4 && ! ${UPLOADSPEED} -ge ${BWLIMITSET} ]]; then
                         if [ -e "${i}" ]; then
                             log "Starting upload of ${i}"
                             # Append filesize to GDSAAMOUNT
