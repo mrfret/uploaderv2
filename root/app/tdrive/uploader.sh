f6fe2b6422677c5289207a0ddf23d9000af55891
doob187 <60312740+doob187@users.noreply.github.com>
2020-11-02 13:05:17 +0100

    Update uploader.sh

--- root/app/tdrive/uploader.sh
+++ root/app/tdrive/uploader.sh
@@ -94,7 +94,7 @@ while true; do
                         UPLOADSPEED=$(vnstat -i eth0 -tr 3 | awk '$1 == "tx" {print $2}' | sed -r 's/([^0-9]*([0-9]*)){1}.*/\2/')
                     fi
                     UPLOADFILE=$(echo $(( ((${BWLIMITSET}-${UPLOADSPEED})) | bc )) | sed -r 's/([^0-9]*([0-9]*)){1}.*/\2/')
-                    MINUPLOADSPEED=$(10)
+                    MINUPLOADSPEED=10
                     if [[ ! ${TRANSFERS} -ge 4 && ! ${UPLOADSPEED} -ge ${BWLIMITSET} && ! {UPLOADFILE} -ge ${MINUPLOADSPEED} ]]; then
                         if [ -e "${i}" ]; then
                             log "Starting upload of ${i}"
